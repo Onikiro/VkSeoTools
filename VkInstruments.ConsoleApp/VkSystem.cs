e0d3d1ddf28d771598281eb04d08106da7320eb4
andobr <andobrydin@gmail.com>
2018-09-06 09:43:31 +0500

    Fixed error in ConsoleApp

--- VkInstruments.ConsoleApp/VkSystem.cs
+++ VkInstruments.ConsoleApp/VkSystem.cs
@@ -17,7 +17,7 @@ namespace VkInstruments.ConsoleApp
             _password = Console.ReadLine();
         }
 
-        public override void Auth()
+        public void Auth()
         {
             SetCredentials();
             bool isLoggedIn;
