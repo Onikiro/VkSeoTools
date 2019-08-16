f3c671988d0036c9c9966701acc7fce6c2888025
Vlad Kabalov <vladskryto@gmail.com>
2019-08-17 00:22:57 +0400

    UserFilterTests and Assert changed to FluentAssertions

---
+++ VkInstruments.Core.Tests/UserFilterTests.cs
@@ -0,0 +1,63 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System.Collections.Generic;
+using VkNet.Enums;
+using VkNet.Model;
+using VkNet.Model.RequestParams;
+
+namespace VkInstruments.Core.Tests
+{
+    [TestFixture]
+    public class UserFilterTests
+    {
+        [Test]
+        public void ApplyFilter()
+        {
+            var users = new List<User>
+            {
+                new User
+                {
+                    Sex = Sex.Male,
+                    City = new City{Id = 1}
+                },
+                new User
+                {
+                    Sex = Sex.Female,
+                    City = new City{Id = 1}
+                },
+                new User
+                {
+                    Sex = Sex.Male,
+                    City = new City{Id = 1}
+                },
+                new User
+                {
+                    Sex = Sex.Female,
+                    City = new City{Id = 2}
+                },
+                new User
+                {
+                    Sex = Sex.Female,
+                    City = new City{Id = 3}
+                },
+            };
+
+            var firstParams = new UserSearchParams
+            {
+                Sex = Sex.Male,
+                City = 1
+            };
+
+            var secondParams = new UserSearchParams
+            {
+                Sex = Sex.Female
+            };
+
+            var firstFilteredUsers = UserFilter.ApplyFilter(users, firstParams);
+            var secondFilteredUsers = UserFilter.ApplyFilter(users, secondParams);
+
+            firstFilteredUsers.Should().HaveCount(2);
+            secondFilteredUsers.Should().HaveCount(3);
+        }
+    }
+}
